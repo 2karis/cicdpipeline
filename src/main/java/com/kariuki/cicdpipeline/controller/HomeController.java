afd121a96c476c78e8df1c2a701d8c04aee2b397
2karis <gitauk2@gmail.com>
2024-03-20 19:06:23 -0400

    updated whole project to use maven

--- src/main/java/com/kariuki/cicdpipeline/controller/HomeController.java
+++ src/main/java/com/kariuki/cicdpipeline/controller/HomeController.java
@@ -1,16 +1,15 @@
-package com.kariuki.cicdpipelline.controller;
+package com.kariuki.cicdpipeline.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/hello")
-public class HelloController {
+public class HomeController {
     @GetMapping
     public ResponseEntity<Map<String,String>> index(){
         return ResponseEntity.ok().body(Map.of("Status","up and running"));
